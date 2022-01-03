741157552f41557a5e0fba60be5b82e1ef714a56
Sinipelto <toni.blafield@tuni.fi>
2022-01-03 22:24:31 +0200

    implement token management UI. fixes to gallery layout.

--- server.js
+++ server.js
@@ -3,10 +3,9 @@ require('dotenv').config({ path: require('find-config')('.env') });
 const utils = require('./utils.js');
 const fs = require('fs');
 const express = require('express');
-const pug = require('pug');
 const mysql = require('mysql');
 
-
+// Init express
 const app = express();
 const host = process.env.NODE_HOST;
 const port = process.env.NODE_PORT;
@@ -14,6 +13,7 @@ const port = process.env.NODE_PORT;
 const photo_url = "/albums";
 const photo_path = __dirname + process.env.PHOTOS_PATH;
 
+// Load static assets
 app.use(photo_url, express.static(photo_path));
 
 app.use('/favicon.ico', express.static(__dirname + process.env.FAVICON_PATH));
@@ -25,29 +25,163 @@ app.use('/plugins', express.static(__dirname + '/node_modules/lightgallery/plugi
 app.use('/fonts', express.static(__dirname + '/node_modules/lightgallery/fonts'));
 app.use('/images', express.static(__dirname + '/node_modules/lightgallery/images'));
 
+// PUG View engine middleware
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-unauthorized = res => {
-	return res.status(401).send("Invalid auth key provided. Ensure parameter 'key' in the URL is set and is correct.");
+// HTTP Body JSON parser middleware
+app.use(express.json());
+
+unauthorized = (err, res) => {
+	return res
+	.status(401).send("Invalid auth key provided." + 
+		" Ensure parameter 'key' in the URL is set and is correct." + 
+		"<br><br>DEBUG INFO: " + err
+		);
 }
 
-const con = () => mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PW,
-	database: process.env.MYSQL_DB
-});
+const con = () => {
+	const conn = mysql.createConnection({
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PW,
+		database: process.env.MYSQL_DB
+	});
+	
+	conn.on('error', err => {
+		console.error("Error during DB connection:", err);
+	});
+	
+	return conn;
+};
 
 app.get('/', (req, res) => {
-	console.log("PATH:", req.path);
-	console.log("QUERY:", req.query);
-	res.sendStatus(404);
+	console.log("REQ PATH:", req.path);
+	console.log("REQ QUERY:", req.query);
+
+	utils.verifyKey(con(), '/', req.query.key, (err, ok) => {
+		if (err || !ok) {
+			console.error(err);
+			unauthorized(err, res);
+		}
+		else {
+			res.render('index', {
+				is_index: true,
+				album_url: photo_url,
+				albums: fs.readdirSync(photo_path)
+			});
+		}
+	});
+});
+
+app.get('/management', (req, res) => {
+	console.log("REQ PATH:", req.path);
+	console.log("REQ QUERY:", req.query);
+
+	utils.verifyKey(con(), '/management', req.query.key, (err, ok) => {
+		if (err || !ok) {
+			console.error(err);
+			unauthorized(err, res);
+		}
+		else {
+			utils.getKeys(con(), null, (err, data) => {
+				if (err || !ok) {
+					console.error(err);
+					res.status(500).send("Failed to get keys from server.");
+				} else {
+					// console.log(data);
+					res.render('manage', {
+						key: req.query.key,
+						data
+					});						
+				}
+			});
+		}
+	});
+});
+
+app.post('/management', (req, res) => {
+	console.log("REQ PATH:", req.path);
+	console.log("REQ QUERY:", req.query);
+	console.log("REQ BODY:", req.body);
+
+	utils.verifyKey(con(), '/management', req.body.key, (err, ok) => {
+		if (err || !ok) {
+			console.error(err);
+			unauthorized(err, res);
+		}
+		else {
+			try {
+				action = req.body.action;
+				target_id = req.body.target_id;
+				data = req.body.data;
+				
+				switch (action) {
+					case "get":
+						utils.getKeys(con(), data.album, (err, result) => {
+							if (err || !ok) {
+								console.error(err);
+								res.status(400).send("ERROR: Failed to get keys:" + err);
+							} else {
+								res.status(200).send(result);
+							}
+						});
+						break;
+					case "new":
+						utils.createKey(con(), data.album, data.usages, (err, ok) => {
+							if (err || !ok) {
+								console.error(err);
+								res.status(400).send("ERROR: Failed to add new key: " + err);
+							} else {
+								res.sendStatus(200);
+							}
+						});
+						break;
+					case "update":
+						utils.updateKey(con(), target_id, data.usages, (err, ok) => {
+							if (err || !ok) {
+								console.error(err);
+								res.status(400).send("ERROR: Failed to modify key:" + err);
+							} else {
+								res.sendStatus(200);
+							}
+						});
+						break;
+					case "revoke":
+						utils.revokeKey(con(), target_id, (err, ok) => {
+							if (err || !ok) {
+								console.error(err);
+								res.status(400).send("ERROR: Failed to revoke key:" + err);
+							} else {
+								res.sendStatus(200);
+							}
+						});
+					case "delete":
+						utils.deleteKey(con(), target_id, (err, ok) => {
+							if (err || !ok) {
+								console.error(err);
+								res.status(400).send("ERROR: Failed to delete key:" + err);
+							} else {
+								res.sendStatus(200);
+							}
+						});
+						break;
+					default:
+						console.error("POST /management: Unknown action request received.");
+						res.status(400).send("ERROR: Unknown action request received.");
+						break;
+				}
+			} catch (err) {
+				console.error("FAILED TO PROCESS POST:", err);
+				res.status(500).send("Caught exception during request processing: " + err);
+			}
+		}
+	});	
 });
 
 app.get('*', (req, res) => {
 	console.log("REQ PATH:", req.path);
-	console.log("REQ QUERY KEY:", req.query.key);
+	console.log("REQ QUERY:", req.query);
 
 	const target_url = photo_url + req.path;
 	const target_path = photo_path + req.path;
@@ -56,7 +190,7 @@ app.get('*', (req, res) => {
 		utils.verifyKey(con(), req.path, req.query.key, (err, ok) => {
 			if (err || !ok) {
 				console.error(err);
-				unauthorized(res)
+				unauthorized(err, res);
 			}
 			else {
 				const files = fs.readdirSync(target_path);
--- utils.js
+++ utils.js
@@ -2,29 +2,126 @@ String.prototype.leftTrim = function() {
     return this.replace(/^\s+/,"");
 }
 
+// Defined on DB side
+// Depends on the keying method, etc.
+const KEY_LENGTH = 32;
 
-const verifyKey = function(con, path, key, callback) {
-	if (key == null || typeof key != 'string') {
+const verifyKey = function(con, album, key, callback) {
+	if (key == null || typeof key != 'string' || key == '' || key.length != KEY_LENGTH) {
 		callback("INVALID_KEY", false);
 		return;
 	}
 
-	const qry_get = `SELECT id, usages, TO_BASE64(HEX(value)) AS result FROM token WHERE album = ? AND TO_BASE64(HEX(value)) = ? AND usages > 0 ORDER BY id LIMIT 1;`;
-	const qry_upd = `UPDATE token SET usages = ? WHERE id = ?`;
+	const qry_get = `SELECT id, album, TO_BASE64(HEX(value)) AS value, usages, created FROM token WHERE album = ? AND TO_BASE64(HEX(value)) = ? AND usages > 0;`;
+	const qry_upd = `UPDATE token SET usages = ? WHERE id = ?;`;
+	
+	try {
+		con.connect(err => {
+			if (err) {
+				console.error(err);
+				callback(err, false);
+			}
+
+			con.query(qry_get, [album, key], (qerr, res) => {
+				if (qerr) {
+					console.error(qerr);
+					callback(qerr, false);
+				}
+				
+				if (res.length <= 0) {
+					console.error("Mathing key not found.");
+					callback("KEY_MATCH_NOT_FOUND", false);
+				} else {
+					row = res[0];
+					con.query(qry_upd, [row.usages - 1, row.id], (qerr, res) => {
+						if (qerr) {
+							console.error(qerr);
+							callback("UPDATE_USAGES_FAILED", false);
+						} else if (row.value == key) {
+							callback(null, true);
+						} else {
+							// This should never occur
+							callback("KEY_VERIFICATION_MISMATCH", false);
+						}
+					});
+				}
+			});
+		});
+	} catch (err) {
+		console.error("Caught exception:", err);
+		callback(err, false);
+	}
+}
+
+const createKey = function(con, album, usages, callback) {
+	if (album == null || typeof album != 'string') {
+		callback("INVALID_ALBUM", false);
+		return;
+	}
+	
+	if (usages == null || typeof usages != 'number') {
+		callback("INVALID_USAGES", false);
+		return;
+	}
+	
+	const qry_get = `SELECT id, album, TO_BASE64(HEX(value)) AS value, usages, created FROM token WHERE ID = ?;`;
+	const qry_new = `INSERT INTO token (album, usages) VALUES (?, ?);`;
 	
 	con.connect(err => {
 		if (err) {
 			console.error(err);
 			callback(err, false);
 		}
-		con.query(qry_get, [path, key], (qerr, res) => {
+
+		con.query(qry_new, [album, (usages != null && usages >= 0) ? usages : 1], (qerr, res) => {
+			if (qerr) {
+				console.error(qerr);
+				callback(qerr, false);
+			}
+			
+			con.query(qry_get, [res.insertId], (qerr, res) => {
+				if (qerr) {
+					console.error(qerr);
+					callback("GET_NEW_KEY_FAILED", false);
+				}
+				if (res.length <= 0) {
+					console.error("Mathing key not found.");
+					callback("NEW_KEY_NOT_FOUND", false);
+				} else {
+					callback(null, true);
+				}
+			});
+		});
+	});
+}
+
+const updateKey = function(con, id, usages, callback) {
+	if (id == null || typeof id != 'number' || isNaN(id) || id < 0) {
+		callback("INVALID_ID", false);
+		return;
+	}
+
+	if (usages == null || typeof usages != 'number' || usages < 0) {
+		callback("INVALID_USAGES", false);
+		return;
+	}
+
+	const qry_get = `SELECT id, album, TO_BASE64(HEX(value)) AS value, usages, created FROM token WHERE id = ?;`;
+	const qry_upd = `UPDATE token SET usages = ? WHERE id = ?;`;
+	
+	con.connect(err => {
+		if (err) {
+			console.error(err);
+			callback(err, false);
+		}
+		con.query(qry_get, [id], (qerr, res) => {
 			if (qerr) {
 				console.error(qerr);
 				callback(qerr, false);
 			}
 			
 			if (res.length <= 0) {
-				console.log("Mathing key not found.");
+				console.error("Mathing key not found.");
 				callback("NOT_FOUND", false);
 			} else {
 				row = res[0];
@@ -40,7 +137,79 @@ const verifyKey = function(con, path, key, callback) {
 			}
 		});
 	});
-};
+}
+
+const revokeKey = function(con, id, callback) {
+	updateKey(con, id, 0, callback);
+}
+
+const deleteKey = function(con, id, callback) {
+	if (id == null || typeof id != 'number' || isNaN(id) || id < 0) {
+		callback("INVALID_ID", false);
+		return;
+	}
+
+	const qry_get = `SELECT id, album, TO_BASE64(HEX(value)) AS value, usages, created FROM token WHERE id = ?;`;
+	const qry_del = `DELETE FROM token WHERE id = ?;`;
+	
+	con.connect(err => {
+		if (err) {
+			console.error(err);
+			callback(err, false);
+		}
+		con.query(qry_get, [id], (qerr, res) => {
+			if (qerr) {
+				console.error(qerr);
+				callback(qerr, false);
+			}
+			
+			if (res.length <= 0) {
+				console.error("Key not found.");
+				callback("KEY_NOT_FOUND", false);
+			} else {
+				row = res[0];
+				con.query(qry_del, [row.id], (qerr, res) => {
+					if (qerr) {
+						console.error(qerr);
+						callback("DELETE_FAILED", false);
+						
+					} else {
+						callback(null, true);
+					}
+				});
+			}
+		});
+	});
+}
+
+const getKeys = function(con, album, callback) {
+	if (album != null && typeof album != 'string') {
+		callback("INVALID_ALBUM", false);
+		return;
+	}
+
+	var qry_get = `SELECT id, album, TO_BASE64(HEX(value)) AS value, usages, created FROM token ${(album == null) ? "" : "WHERE album = ?" };`;
+
+	con.connect(err => {
+		if (err) {
+			console.error(err);
+			callback(err, false);
+		}
+
+		con.query(qry_get, (album == null) ? undefined : [album], (qerr, res) => {
+			if (qerr) {
+				console.error(qerr);
+				callback(qerr, false);
+			}
+			
+			if (res.length <= 0) {
+				console.warning("No keys found.");
+				callback(null, []);
+			} else {
+				callback(null, res);
+			}
+		});
+	});}
 
 filterImages = function(imags) {
 	return imags.filter( elem =>
@@ -56,6 +225,10 @@ filterImages = function(imags) {
 
 module.exports = {
 	String,
+	filterImages,
 	verifyKey,
-	filterImages
+	getKeys,
+	createKey,
+	revokeKey,
+	updateKey,
 }
