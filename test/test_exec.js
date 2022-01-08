d79b1edc74a172e602ca0cff369796f290f3becf
Sinipelto <sinipelto@peltonet.com>
2022-01-08 17:30:02 +0200

    tested exec functionality.

---
+++ test/test_exec.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+
+const serviceName = "node-gallery";
+const logLineLimit = 50;
+
+exec(`journalctl -u ${serviceName} | tail -${logLineLimit}`, (err, stdout, stderr) => {
+	if (err) {
+	        console.error("Failed to execute command:", err);
+                //res.status(500).send("ERROR: Failed to retrieve logs.");
+        } else {
+	        //res.send("STDOUT:<br><br>" + stdout.replace("\r\n", "<br>").replace("\n", "<br>").replace("\\n", "<br>") + "<br><br><br><br>" + "STDE>
+                //res.send(new Buffer(stdout));
+		//console.log(stdout);
+		//str = str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+
+		console.log(stdout.replace(/(?:\r\n|\r|\n)/g, '<br>'));
+        }
+});
