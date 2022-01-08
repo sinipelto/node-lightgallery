e02e858ecfb5ad3609b5a2301f84c0735a6d9e79
Sinipelto <toni.blafield@tuni.fi>
2022-01-08 17:43:56 +0200

    fix: fix prototype definitions.

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-String.prototype.leftTrim = () => this.replace(/^\s+/,'');
+String.prototype.leftTrim = function() { this.replace(/^\s+/,''); };
 
-String.prototype.newLineToHtml = () => this.replace(/(?:\r\n|\r|\n)/g, '<br>');
+String.prototype.newLineToHtml = function() { this.replace(/(?:\r\n|\r|\n)/g, '<br>'); };
 
 const filterMedia = imags => {
 	return imags.filter( elem =>
