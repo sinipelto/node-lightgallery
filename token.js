491fe15ab7c2443cfe42d15a615efef60c5d4023
Sinipelto <toni.blafield@tuni.fi>
2022-08-04 19:24:30 +0300

    implement caching for key querying and verification.

--- token.js
+++ token.js
@@ -2,10 +2,34 @@ const utils = require('./utils.js');
 const activityManager = require('./activity.js');
 
 const fs = require('fs');
+const NodeCache = require('node-cache');
 
 const QUERY_FIELDS = "id, album, TO_BASE64(HEX(value)) AS value, usages_init, usages_left, (usages_init - usages_left) as usages, created";
 const TOKEN_LENGTH = Number(process.env.TOKEN_LENGTH);
 const TABLE = process.env.TOKEN_TABLE;
+
+const CACHE_ENABLED = process.env.ENABLE_KEY_CACHE;
+const CACHE_TTL = Number(process.env.KEY_CACHE_TTL);
+const CACHE_PERIOD = Number(process.env.KEY_CACHE_CHECK_PERIOD);
+const CACHE_CLONES = process.env.KEY_CACHE_CLONES;
+const CACHE_DELETE_EXPIRED = process.env.KEY_CACHE_DELETE_ON_EXPIRE;
+
+if (CACHE_ENABLED === true) {
+	if (isNaN(CACHE_TTL)) {
+		throw "Invalid or missing cache TTL. Check config.";
+	}
+	if (isNaN(CACHE_PERIOD)) {
+		throw "Invalid or missing value KEY_CACHE_CHECK_PERIOD. Check config.";
+	}
+	if (CACHE_CLONES === undefined) {
+		throw "Invalid value for CACHE_CLONES. Check config.";
+	}
+	if (CACHE_DELETE_EXPIRED === undefined || typeof CACHE_DELETE_EXPIRED !== 'boolean') {
+		throw "Invalid value for CACHE_DELETE_EXPIRED. Check config.";
+	}
+} else if (CACHE_ENABLED === undefined) {
+	throw "Invalid value for ENABLE_KEY_CACHE. Check config.";
+}
 
 if (isNaN(TOKEN_LENGTH)) {
 	throw "Invalid key length. Check env var is set.";
@@ -18,7 +42,7 @@ if (!TABLE) {
 const queryGetAll = (album) => `SELECT ${QUERY_FIELDS} FROM ${TABLE} ${(album) ? "WHERE album = ?" : ""};`;
 const queryGetAllExisting = (album) => `SELECT ${QUERY_FIELDS} FROM ${TABLE} ${(album) ? "WHERE album = ? AND" : "WHERE"} deleted IS FALSE;`;
 const queryGetById = () => `SELECT ${QUERY_FIELDS} FROM ${TABLE} WHERE id = ? AND deleted IS FALSE;`;
-const queryGetAllValid = () => `SELECT ${QUERY_FIELDS} FROM ${TABLE} WHERE album = ? AND TO_BASE64(HEX(value)) = ? AND usages_left > 0 AND deleted IS FALSE;`;
+const queryGetValid = () => `SELECT ${QUERY_FIELDS} FROM ${TABLE} WHERE album = ? AND TO_BASE64(HEX(value)) = ? AND usages_left > 0 AND deleted IS FALSE;`;
 
 const queryNew = () => `INSERT INTO ${TABLE} (album, usages_init, usages_left) VALUES (?, ?, ?);`;
 const queryUpdate = (reset) => `UPDATE ${TABLE} SET ${reset ? "usages_init = ?," : ""} usages_left = ? WHERE id = ? AND deleted IS FALSE;`;
@@ -26,6 +50,13 @@ const queryDelete = (filter) => `UPDATE ${TABLE} SET deleted = TRUE ${filter ? "
 
 const validateAlbum = (album) => (typeof album == 'string' && album.startsWith('/'));
 const validateUsages = (usages) => (typeof usages == 'number' && !isNaN(usages) && usages >= 0);
+
+const keyCache = new NodeCache({
+	stdTTL: CACHE_TTL,
+	checkperiod: CACHE_PERIOD,
+	useClones: CACHE_CLONES,
+	deleteOnExpire: CACHE_DELETE_EXPIRED,
+});
 
 module.exports.verifyKey = (con, album, key, info, callback, consume = true) => {
 	if (!con) {
@@ -43,7 +74,15 @@ module.exports.verifyKey = (con, album, key, info, callback, consume = true) => 
 		return;
 	}
 
-	con.query(queryGetAllValid(), [album, key], (qerr, res) => {
+	// If consuming key, we need to query it anyway
+	// If cache enabled, and key found in cache and it matches, simply resp auth ok
+	if (!consume && CACHE_ENABLED && keyCache.has(key) && keyCache.get(key).value == key) {
+		console.debug("Found matching key in cache.");
+		callback(null, true);
+		return;
+	}
+
+	con.query(queryGetValid(), [album, key], (qerr, res) => {
 		if (qerr || !res) {
 			console.error(qerr);
 			callback(qerr, false);
@@ -65,13 +104,17 @@ module.exports.verifyKey = (con, album, key, info, callback, consume = true) => 
 			return;
 		}
 
-		if (consume) {
-			row = res[0];
+		row = res[0];
 
+		if (CACHE_ENABLED) {
+			keyCache.set(row.value, row);
+			keyCache.set(row.id, row);
+		}
+
+		if (consume) {
 			activityManager.addActivity(con, { 'token': row, 'info': info }, (err, ares) => {
 				if (err || !ares) {
 					console.error("ERROR: Failed to log activity:", err);
-					return;
 				}
 			});
 
@@ -228,6 +271,16 @@ module.exports.getKey = (con, id, callback) => {
 		return;
 	}
 
+	if (CACHE_ENABLED && keyCache.has(id)) {
+		const entry = keyCache.get(id);
+		if (entry.id !== id) {
+			callback("KEY_CACHE_ID_MISMATCH", false);
+			return;
+		}
+		callback(null, entry);
+		return;
+	}
+
 	con.query(queryGetById(), [id], (qerr, res) => {
 		if (qerr || !res) {
 			console.error(qerr);
@@ -241,7 +294,16 @@ module.exports.getKey = (con, id, callback) => {
 			return;
 		}
 
-		callback(null, res);
+		// Should NEVER occur!
+		if (res.length > 1) {
+			console.error("ERROR: Multiple key matches found.");
+			callback("MULTIPLE_KEY_MATCHES", false);
+			return;
+		}
+
+		row = res[0];
+
+		callback(null, row);
 	});
 };
 
